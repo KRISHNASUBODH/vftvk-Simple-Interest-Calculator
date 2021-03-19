0e25e683e62a852e0b3c2a8b02851d40c7d92959
SUBODH <krishnasubodh@gmail.com>
2021-03-19 21:47:29 +0530

    added all changed files

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
+    var principal = document.getElementById("principal").value;
+var principalN = parseInt(principal);
+if(principal <= 0) {alert("Enter a positive number")}
+
+var result= document.getElementById("result");
+
+var rate = document.getElementById("rate").value;
+var rateN = parseInt(rate);
+
+var years= document.getElementById("years").value;
+
+var interest = principalN * years * rateN / 100;
+var interestN = parseInt(interest);
+
+var amount = principalN + interestN;
+
+var date = new Date();
+var currentYear = parseInt(date.getFullYear());
+var lastYear = currentYear + parseInt(years);
+
+result.innerHTML = "If you deposit "+principalN + ","+'<br>'+ "at an interest rate of " + rateN +"%."+'<br>' + " You will receive an amount of " + amount + "," +'<br>'+ "in the year " + lastYear + "." ;
+
+
+
+
     
 }
         
